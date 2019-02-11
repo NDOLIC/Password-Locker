5866cc7f934188244558531b07ba76599784af10
NDOLIC <tblaguese1@gmail.com>
2019-02-11 12:38:49 +0200

    changing the names of files

---
+++ User.py
@@ -0,0 +1,16 @@
+class User:
+    """
+    Class that generates new instances of details
+    """
+
+    pass
+
+    User_list = []
+
+    def __init__(self,first_name,last_name,username,password):
+
+        
+        self.first_name = first_name
+        self.last_name = last_name
+        self.Username = username
+        self.Password = password
