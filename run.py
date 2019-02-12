74e66c1be5a73599de71c2497390db03f1c2bdec
NDOLIC <tblaguese1@gmail.com>
2019-02-12 11:02:15 +0200

    changes in main.py

--- run.py
+++ run.py
@@ -82,65 +82,86 @@ from Credentials import Credentials
             print('\n')
 
             while True:
-                    print("Use these short codes : cc - create a new Credentials, dc - display Credentials, fc -find a contact, ex -exit the contact list ")
+                    print("Use these short codes : cc - create a new User,cd - Create a new Credentials dc - display Credentials,sv - Save a Credentials, fc -find a Credentials, ex -exit the Credentials list ")
 
                     short_code = input().lower()
 
                     if short_code == 'cc':
-                            print("New Contact")
+                            print("New User")
                             print("-"*10)
 
-                            print ("First name ....")
-                            f_name = input()
-
-                            print("Last name ...")
-                            l_name = input()
-
-                            print("Phone number ...")
-                            p_number = input()
-
-                            print("Email address ...")
-                            e_address = input()
+                            print ("First_name ....")
+                            first_name = input()
 
 
-                            save_contacts(create_contact(f_name,l_name,p_number,e_address)) # create and save new contact.
+                            print("Last_name ...")
+                            last_name = input()
+
+                            print("User_Name ...")
+                            username = input()
+
+                            print("Password ...")
+                            password = input()
+
+                            save_User(create_User(,,,)) # create and save new user.
                             print ('\n')
-                            print(f"New Contact {f_name} {l_name} created")
+                            print(f"New User {first_name} {last_name} created")
                             print ('\n')
+
+
+                    elif short_code == 'cd':
+                            print("New Credentials")
+                            print("-"*10)
+
+                            print ("Website_Name ....")
+                            site = input()
+
+                            print("User_Name ...")
+                            username = input()
+
+                            print("Password ...")
+                            password = input()
+
+                            save_Credentials(create_Credentials(,,)) # create and save new Credentials.
+                            print ('\n')
+                            print(f"New Credentials {site} { username} created")
+                            print ('\n')
+
+
 
                     elif short_code == 'dc':
 
-                            if display_contacts():
-                                    print("Here is a list of all your contacts")
+                            if display_Credentials():
+                                    print("Your Credentials are:")
                                     print('\n')
 
-                                    for contact in display_contacts():
-                                            print(f"{contact.first_name} {contact.last_name} .....{contact.phone_number}")
+                                    for Credentials in display_Credentials():
+                                            print(f"{Credentials.site} {Credentials.username} {Credentials.password_}")
 
                                     print('\n')
                             else:
                                     print('\n')
-                                    print("You dont seem to have any contacts saved yet")
+                                    print("You are not registered yet")
                                     print('\n')
 
-                    elif short_code == 'fc':
+                    # elif short_code == 'fc':
 
-                            print("Enter the number you want to search for")
+                    #         print("Enter the number you want to search for")
 
-                            search_number = input()
-                            if check_existing_contacts(search_number):
-                                    search_contact = find_contact(search_number)
-                                    print(f"{search_contact.first_name} {search_contact.last_name}")
-                                    print('-' * 20)
+                    #         search_number = input()
+                    #         if check_existing_contacts(search_number):
+                    #                 search_contact = find_contact(search_number)
+                    #                 print(f"{search_contact.first_name} {search_contact.last_name}")
+                    #                 print('-' * 20)
 
-                                    print(f"Phone number.......{search_contact.phone_number}")
-                                    print(f"Email address.......{search_contact.email}")
-                            else:
-                                    print("That contact does not exist")
+                    #                 print(f"Phone number.......{search_contact.phone_number}")
+                    #                 print(f"Email address.......{search_contact.email}")
+                    #         else:
+                    #                 print("That contact does not exist")
 
-                    elif short_code == "ex":
-                            print("Bye .......")
-                            break
-                    else:
-                            print("I really didn't get that. Please use the short codes")
+                    # elif short_code == "ex":
+                    #         print("Bye .......")
+                    #         break
+                    # else:
+                    #         print("I really didn't get that. Please use the short codes")
 
